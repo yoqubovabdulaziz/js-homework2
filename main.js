5a9fe9c0b6a3752fdcfb79b3ad12c722f7e61f5a
yoqubovabdulaziz <abdulazizdasturlash@gmail.com>
2023-12-10 18:25:02 +0500

    finished

---
+++ main.js
@@ -0,0 +1,250 @@
+// Boolean 1
+
+// let A = +prompt("Butun son: ");
+
+// let sum = A % 2 == 0;
+
+// console.log(sum);
+
+// Boolean 2
+
+// let A = +prompt("son-1: ");
+// let B = +prompt("son-2: ");
+
+// let res = A >= 0 || B < -2;
+
+// console.log(res);
+
+// Boolean 3
+
+// Boolean 4
+
+// let A = +prompt("son1");
+// let B = +prompt("son2");
+
+// let res = A % 2 == 1 && B % 2 == 1;
+
+// console.log(res);
+
+// Boolean 5
+
+// let A = +prompt("son1");
+// let B = +prompt("son2");
+
+// let res = A % 2 == 1 || B % 2 == 1
+
+// console.log(res);
+
+// Boolean 6
+
+// let A = +prompt("son1");
+// let B = +prompt("son2");
+
+// let res = (A % 2 == 1 && B % 2 == 0) || (A % 2 == 0 && B % 2 == 1)
+
+// console.log(res);
+
+// Boolean 7
+
+// let A = +prompt("son1");
+// let B = +prompt("son2");
+// let C = +prompt("son3");
+
+// let res = A >= 0 && B >= 0 && C >= 0;
+
+// console.log(res);
+
+// Boolean 8
+
+// let A = +prompt("son1");
+// let B = +prompt("son2");
+// let C = +prompt("son3");
+
+// let res = A >= 0 && B < 0 && C < 0 || A < 0 && B >=0 && C < 0 || A < 0 && B < 0 && C >=0;
+
+// console.log(res);
+
+// Boolean 9
+
+// let A = +prompt("son1");
+// let B = +prompt("son2");
+// let C = +prompt("son3");
+
+// let res = A >= 0 && B >= 0 && C < 0 || A >= 0 && B < 0 && C >= 0 || A < 0 && B >= 0 && C >=0;
+
+// console.log(res);
+
+// Boolean 10
+
+// let A = +prompt("musbat butun son: ");
+
+// let res = (A % 100) % 2 == 0;
+
+// console.log(res);
+
+// Boolean 11
+
+// let abc = +prompt("3 xonali toq son: ")
+
+// let res = abc % 2 == 1;
+
+// console.log(res);
+
+// Boolean 12
+
+// let = A = +prompt("son1: ");
+// let = B = +prompt("son2: ");
+// let = C = +prompt("son3: ");
+
+// let res =
+//   (Math.abs(A) === Math.abs(B) && Math.sign(A) !== Math.sign(B)) ||
+//   (Math.abs(A) === Math.abs(C) && Math.sign(A) !== Math.sign(C)) ||
+//   (Math.abs(C) === Math.abs(B) && Math.sign(C) !== Math.sign(B));
+
+// console.log(res);
+
+// Boolean 13
+
+// let A = +prompt("3 xonali son: ");
+
+// let one = A % 10;
+// let two = (A % 100 - one) / 10;
+// let three = (A - two *10 - one) / 100;
+
+// let res = ((three < two) && (two < one));
+
+// console.log(res);
+
+// Boolean 14
+
+// let A = +prompt("3 xonali son: ");
+
+// let one = A % 10;
+// let two = (A % 100 - one) / 10;
+// let three = (A - two *10 - one) / 100;
+
+// let res = ((three < two) && (two < one)) || ((three > two) && (two > one));
+
+// console.log(res);
+
+// Boolean 15
+
+// let abc = +prompt("3xonali son: ")
+
+// let c = abc % 10;
+// let b = (abc % 100 - c) / 10;
+// let a = (abc - b *10 - c) / 100;
+
+// let res = a == c;
+
+// console.log(res);
+
+
+// OPERATORS
+
+// Operators1
+
+// let A = 5;
+// let B = 8;
+
+// let Y = (-B / 2) * (Math.pow(A, 2) + Math.pow(B, 2));
+
+// console.log(Y);
+
+// Operators2
+
+// let A = 3;
+// let B = 4;
+// let C = 5;
+// let D = 6;
+
+// let Y = Math.pow((A + B / C + D), 2) + Math.pow(B, 2);
+
+// console.log(Y);
+
+// Operators3
+
+// let A = 2;
+// let C = 5;
+// let D = 3;
+
+// let Y = 2 * (Math.pow(A, D) + (4 * Math.pow(C, 2)) / 3);
+
+// console.log(Y);
+
+// Operators4
+
+// let A = 3;
+// let B = 4;
+// let C = 5;
+// let D = 6;
+
+// let Y = 1 / Math.pow(A , 2) * (B / 10)  3 * (C + D)  2;
+
+// console.log(Y);
+
+// Operators5
+
+// let A = 3;
+// let B = 4;
+// let C = 5;
+// let D = 6;
+
+// let Y = Math.pow((((A / B) - 1) / (C / (D - 1))) ,2);
+
+// console.log(Y);
+
+// Operators6
+
+// let A = 3;
+// let B = 4;
+// let C = 5;
+// let D = 6;
+
+// let Y = ((A + B) / (C + D / (A + C)));
+
+// console.log(Y);
+
+// Operators7
+
+// let A = 3;
+// let B = 4;
+// let C = 5;
+
+// let Y = Math.sqrt(Math.pow(A + B + C, 2) - Math.pow(A - B - C, 2));
+
+// console.log(Y);
+
+// Operators8
+
+// let A = 3;
+// let B = 4;
+// let C = 5;
+
+// let Y = ((Math.pow(A , 2) + Math.pow(B , 2) + Math.pow(C , 2)) / (A * B * C));
+
+// console.log(Y);
+
+// Operators9
+
+// let A = 3;
+// let B = 4;
+// let C = 5;
+// let D = 6;
+
+// let Y = A * (Math.pow(D, 3) / 3) + B * (Math.pow(C, 2) / 2);
+
+// console.log(Y);
+
+// Operators10
+
+// let A = 3;
+// let B = 4;
+// let C = 5;
+
+// let Y = Math.abs(A / B + B / C) + Math.pow((A + C), 1 / 4);
+
+// console.log(Y);
+
+
+
